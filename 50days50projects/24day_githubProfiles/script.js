4994b37c27771a9417108a7f6d94ac993152a6a7
Mohit <mohitjangid38437@gmail.com>
2024-01-14 02:01:40 +0530

    day 24 GitHub Profile completed with responsiveness

--- 50days50projects/24day_githubProfiles/script.js
+++ 50days50projects/24day_githubProfiles/script.js
@@ -7,79 +7,87 @@ async function fetchData(apiURL) {
     let data = await response.json();
     return data;
   } catch (error) {
-    return console.log(error.message);
+    return error.message;
   }
 }
 
-async function recentRepos(repoURL) {
-  let repos = await fetch(repoURL);
+async function repositories(repoURL) {
+  let allRepositories = await fetchData(repoURL);
   try {
-    let allRepositories = await repos.json();
-    let repositoryDetails = [];
-
-    allRepositories.forEach((repos, index) => {
-      repositoryDetails.push({
-        repoName: repos.name,
-        reposActiveURL: repos.html_url,
-        reposDate: repos.pushed_at,
-      });
+    let recentRepository = allRepositories.slice(0, 5).map((repos) => {
+      return {
+        name: repos.name,
+        url: repos.html_url,
+      };
     });
-    // let filteredRepos = repositoryDetails.filter((repos, index) => {
-    //   return repos.reposDate.slice(0, 4) > 2023;
-    // });
-    let filteredRepos = repositoryDetails.map((repos) => {
-      repos;
-    });
-    console.log(filteredRepos);
-    // return data;
+    return recentRepository;
   } catch (error) {
-    return console.log(error.message);
+    return (error.message = "Repository Not Found");
   }
 }
 
-recentRepos("https://api.github.com/users/itz-mohit-014/repos");
-
-async function findUser(e) {
-  let userName = e.currentTarget.value;
+async function showCard(e) {
+  let inputName = e.currentTarget.value;
   e.currentTarget.value = "";
-  const apiURL = `https://api.github.com/users/${userName}`;
-
+  const apiURL = `https://api.github.com/users/${inputName} `;
   let profileData = await fetchData(apiURL);
-  let profileImg = profileData.avatar_url;
-  let name = profileData.name;
-  let bio = profileData.bio;
-  let follower = profileData.followers;
-  let following = profileData.following;
-  let repoURL = profileData.repos_url;
-  let recentRepository = await recentRepos(repoURL);
+  let userName = profileData.name;
+  try {
+    profileCard.classList.add("show");
+    profileCard.innerHTML = "";
+    let recentRepository = await repositories(
+      profileData.repos_url + "?sort=created"
+    );
 
-  // recentRepos(repos);
-  let cardContent = ` 
-      <div class="profile-pic left">
-      <img src="../profile13.jpg" alt="userProfile" />
-    </div>
-    <div class="user-details right">
-      <h1 class="user-name">Mohit</h1>
-      <p class="user-about">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
-        molestias laboriosam consequuntur nesciunt earum sapiente
-        reprehenderit, excepturi sint explicabo aliquam?
-      </p>
-      <div class="follower-container">
-        <p>16562 <strong>Followers</strong></p>
-        <p>0 <strong>Following</strong></p>
-        <p>20 <strong>Repos</strong></p>
-      </div>
-      <div class="tags-container">
-        <p class="tags">Lorem, ipsum.</p>
-        <p class="tags">Lorem, ipsum. Lorem</p>
-        <p class="tags">Lorem, ipsum. tags</p>
-      </div>
-    </div>
-    
-    `;
-  profileCard.classList.add("show");
-  profileCard.innerHTML = cardContent;
+    createProfileCard(profileData, userName);
+    appendRepository(recentRepository);
+  } catch (error) {
+    profileCard.innerHTML = "User Not Found";
+  }
 }
 
-input.addEventListener("change", findUser);
+function createProfileCard(profileData, userName) {
+  const userProfileContainer = document.createElement("div");
+  userProfileContainer.className = "profile-pic left center";
+  userProfileContainer.innerHTML = `
+    <img src="${profileData.avatar_url}" alt="userProfile" />`;
+
+  const cardRightDetails = document.createElement("div");
+  cardRightDetails.className = "user-details right";
+  cardRightDetails.innerHTML = `
+    <h1 class="user-name"><a href="${profileData.html_url}" target="_blank">
+    ${userName.slice(0, 1).toUpperCase() + userName.slice(1)}
+    </a>
+    </h1>
+    
+    <p class="user-about">
+    ${profileData.bio || ""}
+    </p>
+    
+    <div class="follower-container">
+    <p>${profileData.followers} <strong>Followers</strong></p>
+    <p>${profileData.following} <strong>Following</strong></p>
+    <p>${profileData.public_repos} <strong>Repos</strong></p>
+    </div>
+    `;
+
+  profileCard.append(userProfileContainer);
+  profileCard.append(cardRightDetails);
+}
+
+function appendRepository(repositoryData) {
+  const repoContainer = document.createElement("div");
+  repoContainer.classList.add("tags-container");
+  repositoryData.forEach((repo) => {
+    const tagEl = document.createElement("a");
+    tagEl.href = repo.url;
+    tagEl.target = "_blank";
+    tagEl.classList.add("tags");
+    tagEl.innerText = repo.name;
+    repoContainer.append(tagEl);
+  });
+
+  profileCard.children[1].append(repoContainer);
+}
+
+input.addEventListener("change", showCard);
