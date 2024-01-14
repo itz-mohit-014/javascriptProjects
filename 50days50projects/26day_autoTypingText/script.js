2d527bfdc86133afe7f64969674d6cb0f4f74a74
Mohit <mohitjangid38437@gmail.com>
2024-01-14 15:57:01 +0530

    day26 adding auto typing text added

--- 50days50projects/26day_autoTypingText/script.js
+++ 50days50projects/26day_autoTypingText/script.js
@@ -1,1 +1,30 @@
+const dynamicTextEl = document.getElementById("dynamic-text");
+const inputSpeed = document.querySelector("input");
+const text = "Welcome to the Programming World!";
+let startChar = 0;
+let initialSpeed = 350 / inputSpeed.value;
+let intervalId;
 
+function dynmicText(str) {
+  const letters = str.split("");
+  addingText(letters);
+}
+
+function addingText(arrayOfLetters) {
+  intervalId = setInterval(() => {
+    dynamicTextEl.textContent += arrayOfLetters[startChar];
+    startChar++;
+    if (startChar == arrayOfLetters.length + 1) {
+      startChar = 0;
+      dynamicTextEl.textContent = "";
+    }
+  }, initialSpeed);
+}
+
+inputSpeed.addEventListener("input", (e) => {
+  initialSpeed = 350 / e.target.value;
+  clearInterval(intervalId);
+  dynmicText(text);
+});
+
+dynmicText(text);
