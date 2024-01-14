9cf4eab6c52f5aceb4c834bbadc5996357ce4c35
Mohit <mohitjangid38437@gmail.com>
2024-01-14 11:17:09 +0530

    day24 Double Click Heart added

--- 50days50projects/25day_doubleClickHeart/script.js
+++ 50days50projects/25day_doubleClickHeart/script.js
@@ -1,1 +1,22 @@
+const likeCount = document.getElementById("likeCount");
+const picture = document.querySelector(".picture");
 
+let clickCount = 0;
+function showLike(e) {
+  const likeIcon = document.createElement("i");
+  likeIcon.className = "fa-solid fa-heart like-icon";
+  picture.append(likeIcon);
+
+  likeIcon.style.left = `${e.offsetX}px`;
+  likeIcon.style.top = `${e.offsetY}px`;
+
+  clickCount++;
+  likeCount.innerHTML = clickCount;
+
+  this.classList.add("liked");
+  setTimeout(() => {
+    likeIcon.remove("liked");
+  }, 500);
+}
+
+picture.addEventListener("dblclick", showLike);
