7fb655e915e99b081cb5a60f853853f8abd5a781
Mohit <mohitjangid38437@gmail.com>
2024-01-16 00:58:16 +0530

    day 30 Animated Countdown added

--- 50days50projects/30day_animatedCountdown/script.js
+++ 50days50projects/30day_animatedCountdown/script.js
@@ -1,9 +1,36 @@
 const countdown = document.querySelector(".countdown");
+const finalEl = document.querySelector(".final");
+const replayBtn = document.getElementById("replay");
+
+function toggle() {
+  countdown.parentElement.classList.toggle("hide");
+  finalEl.classList.toggle("hide");
+}
+
 let countdownStart = 3;
-let intervalId = setInterval(() => {
+let timeoutId;
+let intervalId;
+function updateCountdown() {
   countdown.textContent = countdownStart;
   countdownStart--;
   if (countdownStart < 0) {
     countdownStart = 3;
   }
-}, 1000);
+}
+
+function controlTimingFunc() {
+  timeoutId = setTimeout(() => {
+    clearInterval(intervalId);
+    toggle();
+  }, 4850);
+
+  intervalId = setInterval(updateCountdown, 1200);
+}
+
+updateCountdown();
+controlTimingFunc();
+
+replayBtn.addEventListener("click", () => {
+  toggle();
+  controlTimingFunc();
+});
